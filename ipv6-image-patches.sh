6e7919dba5a0c75476fa27ad3efa969f4afbf278
Harald Jensås <hjensas@redhat.com>
2020-03-06 08:35:34 +0100

    Fix paths

--- ipv6-image-patches.sh
+++ ipv6-image-patches.sh
@@ -5,17 +5,17 @@ set -e
 #
 # Patch cloud-init in built image
 #
-cd /home/centos
+cd ~
 
 git clone -b bug/1806014 https://github.com/hjensas/cloud-init.git
 
-LIBGUESTFS_BACKEND=direct virt-customize --add /home/centos/images/overcloud-full.qcow2 \
+LIBGUESTFS_BACKEND=direct virt-customize --add ~/images/overcloud-full.qcow2 \
   --delete /usr/lib/python2.7/site-packages/cloudinit \
   --selinux-relabel
-LIBGUESTFS_BACKEND=direct virt-customize --add /home/centos/images/overcloud-full.qcow2 \
+LIBGUESTFS_BACKEND=direct virt-customize --add ~/images/overcloud-full.qcow2 \
   --copy-in /home/centos/cloud-init/cloudinit:/usr/lib/python2.7/site-packages/ \
   --selinux-relabel
-LIBGUESTFS_BACKEND=direct virt-customize --add /home/centos/images/overcloud-full.qcow2 \
+LIBGUESTFS_BACKEND=direct virt-customize --add ~/images/overcloud-full.qcow2 \
   --delete /usr/lib/python2.7/site-packages/cloudinit/tests \
   --selinux-relabel
 
