a6fd1b39a881ffabe8a0dc4ebb60456aa11c6834
Harald Jensås <hjensas@redhat.com>
2020-03-07 12:26:58 +0100

    Update image patching task

--- ipv6-image-patches.sh
+++ ipv6-image-patches.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-#
-# Patch cloud-init in built image
-#
-cd ~
+USER_HOME=/home/cloud-user
 
-
-LIBGUESTFS_BACKEND=direct virt-customize --add ~/images/overcloud-full.qcow2 \
-  --upload /opt/local_repo/cloud-init-18.5-5.el8_1.eterrell202003061044.noarch.rpm:/root \
+#
+# Patch cloud-init in image
+#
+LIBGUESTFS_BACKEND=direct virt-customize --add $USER_HOME/images/overcloud-full.qcow2 \
+  --upload /opt/local_repo/cloud-init-18.5-11.el8.eterrell202003061040.noarch.rpm:/root \
   --selinux-relabel
-# LIBGUESTFS_BACKEND=direct virt-customize --add ~/images/overcloud-full.qcow2 \
+# LIBGUESTFS_BACKEND=direct virt-customize --add $USER_HOME/images/overcloud-full.qcow2 \
 #   ----run-command "dnf update /root/cloud-init-18.5-11.el8.eterrell202003061040.noarch.rpm" \
 #   --selinux-relabel
 
