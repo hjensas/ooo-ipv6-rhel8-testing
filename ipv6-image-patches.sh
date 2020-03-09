ac39400cf43cbb76983c8555103383f170217240
Harald Jensås <hjensas@redhat.com>
2020-03-09 23:23:44 +0100

    Mova patch image to ansible

--- ipv6-image-patches.sh
+++ ipv6-image-patches.sh
@@ -9,10 +9,8 @@ USER_HOME=/home/cloud-user
 #
 LIBGUESTFS_BACKEND=direct virt-customize --add $USER_HOME/images/overcloud-full.qcow2 \
   --upload /opt/local_repo/cloud-init-18.5-11.el8.eterrell202003061040.noarch.rpm:/root \
+  --run-command "dnf update /root/cloud-init-18.5-11.el8.eterrell202003061040.noarch.rpm \
   --selinux-relabel
-# LIBGUESTFS_BACKEND=direct virt-customize --add $USER_HOME/images/overcloud-full.qcow2 \
-#   ----run-command "dnf update /root/cloud-init-18.5-11.el8.eterrell202003061040.noarch.rpm" \
-#   --selinux-relabel
 
 echo "DONE patching image"
 
