a257ad7b0bf6f02974debb73a19aa3cd9a615ba6
Harald Jensås <hjensas@redhat.com>
2020-03-09 17:47:07 +0100

    Fix path in deploy command

--- overcloud/deploy_overcloud.sh
+++ overcloud/deploy_overcloud.sh
@@ -12,6 +12,6 @@ openstack overcloud deploy \
   -e ~/overcloud/environment/node_data.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
-  -e /usr/share/openstack/tripleo-heat-templates/environments/net-multiple-nics.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/net-multiple-nics.yaml \
   -e ~/overcloud/environment/network-environment-overrides.yaml
 
